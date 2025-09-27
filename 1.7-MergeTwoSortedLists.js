3b0e5952ceedb055eb806a2877ba41b561a9e538
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2025-09-27 22:46:16 +0300

    update
    -starting fe

---
+++ 1.7-MergeTwoSortedLists.js
@@ -0,0 +1,7 @@
+const testCases = [[[1,2,4], [1,3,4]], [[], []], [[], [0]]];
+
+const mergeTwoSertedLists = (lists) => {
+    
+}
+
+testCases.forEach(test => console.log(mergeTwoSertedLists(test)));
