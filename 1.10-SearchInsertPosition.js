7cf128022d716f79592bb955f7bf651ba8d8acfd
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2025-11-06 22:24:43 +0300

    Implement binary search for insert position

---
+++ 1.10-SearchInsertPosition.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let low = 0;
+    let high = nums.length - 1;
+    let mid;
+    while(low <= high) {
+        mid = Math.floor(low + (high - low) / 2);
+        if(nums[mid] === target) {
+            return mid;
+        } else {
+            if(nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+    }
+
+    return low;
+};
